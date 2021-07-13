99acb2e6a491b8e9f7b553911eb2ec5f44f5d862
Maria Cassandra Gugol <mcmgugol@gmail.com>
2021-07-13 10:12:05 -0700

    adding more HTML, create CSS, and Javascript functions

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,55 @@
+function startGame(){
+
+}
+
+function askNextQuestion(){
+
+}
+
+function Response(){
+    
+}
+
+
+// teacher demo this code structure in office hours:
+var correct=0;
+var incorrect=0;
+// questions for the quiz
+var questions=[
+    {
+    questionText:"What is a variable?",
+    answers:["A keyword assigned a value to hold information", "A vacation", "a suitcase", "a letter"],
+    correctAnswerIndex:0,
+    },
+    {
+    questionText:"Which of the following are data types?",
+    answers:["String", "Boolean", "Number", "All of the above"],
+    correctAnswerIndex:3,
+    },
+    {
+    questionText:"You can store groups of data in a single variable called a/an",
+    answers:["method", "array", "console", "css"],
+    correctAnswerIndex:1,
+    }
+]
+
+// Loop through the questions and get user's response
+for (var i = 0; i<questions.length; i++){
+    var response=showQuestion(questions[i].questionText)
+    if (reponse ==questions[i].answers){
+        correct++;
+        alert("correct");
+    }else (reponse !=questions[i].answers){
+        incorrect--;
+        alert("wrong");
+    }
+}
+
+
+
+
+
+
 var startButton = document.querySelector("#startButton");
 
 startButton.addEventListener("click", function(){
