f9191666ef72dec8a5185f6f0be923a9cdaa28bc
Maria Cassandra Gugol <mcmgugol@gmail.com>
2021-07-14 11:11:33 -0700

    displaying the question

--- assets/script.js
+++ assets/script.js
@@ -1,57 +1,70 @@
-function startGame(){
+var startButton = document.querySelector("#startButton");
+var questionElement = document.getElementById("questions") 
+var answerButtons = document.querySelectorAll ("button")
 
-}
+startButton.addEventListener("click", startQuiz)
 
-function askNextQuestion(){
-
-}
-
-function Response(){
-    
-}
-
+let currentQuestionIndex   
 
 // teacher demo this code structure in office hours:
-var correct=0;
-var incorrect=0;
+var score=0;
 // questions for the quiz
 var questions=[
     {
     questionText:"What is a variable?",
     answers:["A keyword assigned a value to hold information", "A vacation", "a suitcase", "a letter"],
-    correctAnswerIndex:0,
+    correct:"A keyword assigned a value to hold information"
     },
     {
     questionText:"Which of the following are data types?",
     answers:["String", "Boolean", "Number", "All of the above"],
-    correctAnswerIndex:3,
+    correct:"All of the above"
     },
     {
     questionText:"You can store groups of data in a single variable called a/an",
     answers:["method", "array", "console", "css"],
-    correctAnswerIndex:1,
+    correct:"array"
     }
 ]
 
-// Loop through the questions and get user's response
-for (var i = 0; i<questions.length; i++){
-    var response=showQuestion(questions[i].questionText)
-    if (reponse ==questions[i].answers){
-        correct++;
-        alert("correct");
-    }else (reponse !=questions[i].answers){
-        incorrect--;
-        alert("wrong");
-    }
+
+
+console.log(questions[0].questionText)
+
+function startQuiz(){
+    console.log ("started")
+    currentQuestionIndex=0
+    placeQuestionsOnPage(currentQuestionIndex)
+
 }
 
+// function askQuestion(){
+//     placeQuestionsOnPage(questions[currentQuestionIndex])
+
+// }
+
+function placeQuestionsOnPage(index){
+    var questionText = questions[index].questionText;
+    // var answers= questions.answers;
+    // var correctAnswerIndex= questions.correctAnswerIndex;
+
+    var questionEl=document.getElementById("questions")
+    questionEl.innerHTML=questionText
+}
+
+// function chooseAnswer(){
+
+// }
+
+// Loop through the questions and get user's response
+for (let i = 0; i<questions.length; i++){
+    var response=placeQuestionsOnPage(questions[i].questionText)
+    if (reponse === questions.correctAnswerIndex){
+        score++;
+        alert("correct");
+    }else (reponse !=questions[i].answers)
+        score--;
+        alert("wrong");
+    }
 
 
-
-
-
-var startButton = document.querySelector("#startButton");
-
-startButton.addEventListener("click", function(){
-    // if start timer is clicked start quiz and start timer
-})
