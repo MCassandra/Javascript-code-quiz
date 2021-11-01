4c8b38adb7547645e631764162bc02f91d70444d
Maria Cassandra Gugol <mcmgugol@gmail.com>
2021-11-01 13:58:15 -0700

    fixing score functions

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,13 @@
 // elements i need to grab 
 var timerElement = document.querySelector(".timer-count");
 var startButton = document.querySelector("#startButton");
+var score = document.querySelector(".score");
+
 startButton.addEventListener("click", startQuiz);
 
 var timer;
 var timerCount;
+var score;
 let currentQuestionIndex
 
 // questions for the quiz
@@ -23,18 +26,41 @@ var questions = [
         questionText: "You can store groups of data in a single variable called a/an",
         answers: ["method", "array", "console", "css"],
         correct: "array"
-    }
+    },
+    {
+        questionText: "True or False: A string is a series of characters and is surrounded by quotes.",
+        answers: ["true", "false"],
+        correct: "true"
+    },
+    {
+        questionText: "True or False: Booleans have one value",
+        answers: ["true: booleans have one value", "false: booleans have true and false values"],
+        correct: "false: booleans have true and false values"
+    },
+    {
+        questionText: "What does a modulus operator (%) return?",
+        answers: ["remainder between two numbers", "the sum of two numbers", "the difference of two numbers", "console.log value"],
+        correct: "remainder between two numbers"
+    },
+
 ]
+// The init function is called when the page loads 
+function init() {
+    getScores();
+  }
+
 
 // start the quiz
 function startQuiz() {
     console.log("started")
     currentQuestionIndex = 0;
-    timerCount = 10;
+    timerCount = 60;
+    score = 0;
     placeQuestionsOnPage(currentQuestionIndex);
     placeAnswersOnPage(currentQuestionIndex);
     startButton.disabled = true;
     startTimer();
+    dislplayScore();
 }
 
 
@@ -59,25 +85,53 @@ function placeAnswersOnPage(index) {
 
 };
 
+function dislplayScore() {
+    var scoreEl = document.querySelector(".score");
+    scoreEl.textContent = score;
+
+};
+
 function checkAnswer(event) {
+    
     const selectAnswer = questions[currentQuestionIndex].correct;
     if (event.target.textContent === selectAnswer) {
         currentQuestionIndex++;
         if (currentQuestionIndex >= questions.length) {
-            console.log("game over");
+            console.log("congrats!");
         }
         else {
             placeQuestionsOnPage(currentQuestionIndex);
             placeAnswersOnPage(currentQuestionIndex);
         }
+        score++;
     }
 };
 
+// Updates score count on screen and sets score to client storage
+function setScore() {
+    score.textContent = scoreCounter;
+    localStorage.setItem("quizScore", quizScore);
+  }
+
+// These functions are used by init
+function getScore() {
+    // Get stored value from client storage, if it exists
+    var storedScore = localStorage.getItem("quizScore");
+    // If stored value doesn't exist, set counter to 0
+    if (storedScore === null) {
+        quizScore = 0;
+    } else {
+      // If a value is retrieved from client storage set the quizScore to that value
+      quizScore = storedScore;
+    }
+    //Render score count to page
+    score.textContent = scoreCounter;
+  }
 
 var selectAnswer = document.querySelector(".answersContainer");
 selectAnswer.addEventListener("click", checkAnswer);
 
-// // The setTimer function starts and stops the timer and triggers winGame() and loseGame()
+// // The setTimer function starts and stops the timer and triggers startQuiz() and endQuiz()
 function startTimer() {
     // Sets timer
     timer = setInterval(function() {
@@ -99,5 +153,8 @@ function startTimer() {
       }
     }, 1000);
   };
+
+  init();
+
 
 
